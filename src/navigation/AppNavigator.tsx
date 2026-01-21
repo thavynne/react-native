4d9d8fc7bc562117aa40d0453e08cc8c6f0f9de7
thavynne <thavynneluzo2000@gmail.com>
2026-01-21 15:46:45 -0300

    primeiro commit

---
+++ src/navigation/AppNavigator.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from '../screens/HomeScreen';
+import DetailsScreen from '../screens/DetailsScreen';
+
+export type RootStackParamList = {
+  Home: undefined;
+  Details: undefined;
+  Register: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export default function AppNavigator() {
+  return (
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{ title: 'Início' }}
+      />
+      <Stack.Screen
+        name="Details"
+        component={DetailsScreen}
+        options={{ title: 'Detalhes' }}
+      />
+      <Stack.Screen
+        name="Register"
+        component={RegisterScreen}
+        options={{ title:'Cadastro' }}
+      />
+    </Stack.Navigator>
+  );
+}
